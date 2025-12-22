2fb0c1ce438857c73891289d4e103c72ffa292b8
S-Marnat <242020513+S-Marnat@users.noreply.github.com>
2025-12-22 15:27:55 +0100

    test: ajout des tests unitaires pour la gestion des langues

---
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/P3AddNewFunctionalityDotNetCore.UnitTests/LanguageServiceTests.cs
@@ -0,0 +1,58 @@
+﻿using P3AddNewFunctionalityDotNetCore.Models.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace P3AddNewFunctionalityDotNetCore.Tests.P3AddNewFunctionalityDotNetCore.UnitTests
+{
+    public class LanguageServiceTests
+    {
+        [Fact]
+        public void SetCulture_GetCultureFrench_ReturnFr()
+        {
+            // Arrange
+            ILanguageService languageService = new LanguageService();
+            string language = "French";
+
+            // Act
+            string culture = languageService.SetCulture(language);
+
+            // Assert
+            Assert.Same("fr", culture);
+        }
+
+        [Fact]
+        public void SetCulture_GetCultureEnglishAndDefault_ReturnEn()
+        {
+            // Arrange
+            ILanguageService languageService = new LanguageService();
+            string language = "English";
+            string languageDefault = "";
+
+            // Act
+            string culture = languageService.SetCulture(language);
+            string cultureDefault = languageService.SetCulture(languageDefault);
+
+            // Assert
+            Assert.Same("en", culture);
+            Assert.Same("en", cultureDefault);
+        }
+
+        [Fact]
+        public void SetCulture_GetCultureSpanish_ReturnEs()
+        {
+            // Arrange
+            ILanguageService languageService = new LanguageService();
+            string language = "Spanish";
+
+            // Act
+            string culture = languageService.SetCulture(language);
+
+            // Assert
+            Assert.Same("es", culture);
+        }
+    }
+}
