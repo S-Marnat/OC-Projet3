81773cf5c92f3e2ed1113eb4b756703091308cd7
S-Marnat <242020513+S-Marnat@users.noreply.github.com>
2025-12-31 15:39:08 +0100

    test(orderService): ajout des tests unitairers pour la classe OrderService

---
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/P3AddNewFunctionalityDotNetCore.UnitTests/OrderServiceTests.cs
@@ -0,0 +1,153 @@
+﻿using Microsoft.Extensions.Localization;
+using Moq;
+using P3AddNewFunctionalityDotNetCore.Models;
+using P3AddNewFunctionalityDotNetCore.Models.Entities;
+using P3AddNewFunctionalityDotNetCore.Models.Repositories;
+using P3AddNewFunctionalityDotNetCore.Models.Services;
+using P3AddNewFunctionalityDotNetCore.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace P3AddNewFunctionalityDotNetCore.Tests.P3AddNewFunctionalityDotNetCore.UnitTests
+{
+    public class OrderServiceTests
+    {
+        // Utilisation d'un constructeur pour éviter de recréer des services avec les mocks pour chaque test
+
+        private readonly Mock<ICart> _mockCart;
+        private readonly Mock<IOrderRepository> _mockOrderRepository;
+        private readonly Mock<IProductService> _mockProductService;
+
+        private readonly OrderService _orderService;
+
+        public OrderServiceTests()
+        {
+            // Initialisation des mocks
+            _mockCart = new Mock<ICart>();
+            _mockOrderRepository = new Mock<IOrderRepository>();
+            _mockProductService = new Mock<IProductService>();
+
+            // Création du service avec les mocks
+            _orderService = new OrderService(
+                _mockCart.Object,
+                _mockOrderRepository.Object,
+                _mockProductService.Object
+            );
+        }
+
+        [Fact]
+        // Test d'une méthode asynchrone qui renvoie une tâche => public async Task
+        public async Task GetOrder_WhenGetOrderIsCalled_GetOrderCalled1TimeAndReturnOrderId1()
+        {
+            // Arrange
+            int id = 1;
+
+            // Création d'un Order pour simuler ce que renverrait le repository
+            var expectedOrder = new Order { Id = id };
+
+            // Utilisation d'un .ReturnsAsync pour tester une méthode asynchrone
+            _mockOrderRepository.Setup(r => r.GetOrder(id))
+                                .ReturnsAsync(expectedOrder);
+
+            // Act
+            // Attente de la Task, comme dans le vrai code => await
+            var returnedOrder = await _orderService.GetOrder(id);
+
+            // Assert
+            _mockOrderRepository.Verify(r => r.GetOrder(id), Times.Once);
+            Assert.Equal(expectedOrder, returnedOrder);
+        }
+
+        [Fact]
+        public async Task GetOrders_WhenGetOrdersIsCalled_GetOrdersCalled1TimeReturn2OrdersId1AndId2()
+        {
+            // Arrange
+            int id1 = 1;
+            int id2 = 2;
+
+            var expectedOrder1 = new Order {Id = id1};
+            var expectedOrder2 = new Order {Id = id2};
+
+            _mockOrderRepository.Setup(r => r.GetOrders())
+                                .ReturnsAsync(new List<Order> { expectedOrder1, expectedOrder2 });
+
+            // Act
+            var returnedOrder = await _orderService.GetOrders();
+
+            // Assert
+            _mockOrderRepository.Verify(r => r.GetOrders(), Times.Once);
+
+            Assert.Equal(2, returnedOrder.Count);
+
+            // Vérification du contenu de la liste retournée
+            Assert.Contains(expectedOrder1, returnedOrder);
+            Assert.Contains(expectedOrder2, returnedOrder);
+        }
+
+        [Fact]
+        public void SaveOrder_WhenSaveOrderIsCalled_CallsMapToOrderEntityAndSaveOrderAndUpdateInventory()
+        {
+            // Arrange
+            // Création d'un panier
+            var cartLine = new List<CartLine>()
+            {
+                new CartLine
+                {
+                    Product = new Product { Id = 1 },
+                    Quantity = 2
+                }
+            };
+
+            // Création d'une commande
+            var order = new OrderViewModel
+            {
+                Lines = cartLine,
+                Name = "Mon nom",
+                Address = "Mon adresse",
+                City = "Ma ville",
+                Zip = "12345",
+                Country = "Mon pays"
+            };
+
+            // Récupération de l'Order envoyé au repository
+            Order saveOrder = null;
+
+            _mockOrderRepository.Setup(r => r.Save(It.IsAny<Order>()))
+                                .Callback<Order>(o => saveOrder = o);
+
+            // Act
+            _orderService.SaveOrder(order);
+
+            /* Transformation des informations de la commande en liste :
+               OrderLine est une ICollection => pas d'indexation directe possible */
+            var lines = saveOrder.OrderLine.ToList();
+
+            // Assert
+            // Vérification que Save est appelé une seule fois
+            _mockOrderRepository.Verify(r => r.Save(It.IsAny<Order>()), Times.Once);
+
+            // Vérification que l'Order n'est pas vide
+            Assert.NotNull(saveOrder);
+
+            // Vérification que les informations du formulaire sont mappées correctement
+            Assert.Equal("Mon nom", saveOrder.Name);
+            Assert.Equal("Mon adresse", saveOrder.Address);
+            Assert.Equal("Ma ville", saveOrder.City);
+            Assert.Equal("12345", saveOrder.Zip);
+            Assert.Equal("Mon pays", saveOrder.Country);
+
+            // Vérification que les informations de la commande sont mappées correctement
+            Assert.Single(saveOrder.OrderLine);
+            Assert.Equal(1, lines[0].ProductId);
+            Assert.Equal(2, lines[0].Quantity);
+
+            // Vérification que UpdateProductQuantities et Clear sont bien appelées une seule fois
+            _mockProductService.Verify(r => r.UpdateProductQuantities(), Times.Once);
+            _mockCart.Verify(r => r.Clear(), Times.Once);
+        }
+    }
+}
