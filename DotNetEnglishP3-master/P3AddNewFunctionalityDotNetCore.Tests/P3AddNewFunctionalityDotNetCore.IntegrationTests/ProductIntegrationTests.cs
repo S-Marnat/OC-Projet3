281bcdf4c5559d9c9f7901948e96df8590a64927
S-Marnat <242020513+S-Marnat@users.noreply.github.com>
2026-01-15 16:25:11 +0100

    test: amélioration et suppression de tests d'intégration

--- DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/P3AddNewFunctionalityDotNetCore.IntegrationTests/ProductIntegrationTests.cs
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/P3AddNewFunctionalityDotNetCore.IntegrationTests/ProductIntegrationTests.cs
@@ -24,23 +24,19 @@ public class ProductIntegrationTests
     public async Task CreateProduct_SaveNewProduct_NewProductInInventory()
     {
         // Arrange
-        // Création d'un DbContext EF Core qui pointe vers la base SQL Server
         P3Referential context = CreateDbContext();
         Product product = null;
 
         try
         {
-            // Instanciation des service et repository utilisés
             LanguageService languageService = new LanguageService();
             Cart cart = new Cart();
             ProductRepository productRepository = new ProductRepository(context);
             OrderRepository orderRepository = new OrderRepository(context);
             ProductService productService = new ProductService(cart, productRepository, orderRepository, _localizerService);
 
-            // Instanciation du contrôleur
             ProductController productController = new ProductController(productService, languageService, _localizerController);
 
-            // Création d'un article à travers le formulaire
             ProductViewModel productViewModel = new ProductViewModel()
             {
                 Name = "Le produit a sauvegarder",
@@ -50,7 +46,6 @@ public class ProductIntegrationTests
                 Details = "Les détails du produit"
             };
 
-            // Détermination du nombre initial de produits
             int productsBefore = await context.Product.CountAsync();
 
             // Act
@@ -66,7 +61,6 @@ public class ProductIntegrationTests
         }
         finally
         {
-            // Nettoyage de la base
             if (product != null)
                 await CleanTestData(context, product);
         }
@@ -76,48 +70,58 @@ public class ProductIntegrationTests
     public async Task DeleteProduct_DeleteNewProduct_NewProductNotInInventory()
     {
         // Arrange
-        // Création d'un DbContext EF Core qui pointe vers la base SQL Server
         P3Referential context = CreateDbContext();
+        Product product = null;
 
-        // Instanciation des service et repository utilisés
-        LanguageService languageService = new LanguageService();
-        Cart cart = new Cart();
-        ProductRepository productRepository = new ProductRepository(context);
-        OrderRepository orderRepository = new OrderRepository(context);
-        ProductService productService = new ProductService(cart, productRepository, orderRepository, _localizerService);
-
-        // Instanciation du contrôleur
-        ProductController productController = new ProductController(productService, languageService, _localizerController);
-
-        // Création d'un article à travers le formulaire
-        ProductViewModel productViewModel = new ProductViewModel()
+        try
         {
-            Name = "Le produit a supprimer",
-            Price = "9.99",
-            Stock = "10",
-            Description = "La description du produit",
-            Details = "Les détails du produit"
-        };
+            LanguageService languageService = new LanguageService();
+            Cart cart = new Cart();
+            ProductRepository productRepository = new ProductRepository(context);
+            OrderRepository orderRepository = new OrderRepository(context);
+            ProductService productService = new ProductService(cart, productRepository, orderRepository, _localizerService);
 
-        // Détermination du nombre initial de produits
-        int productsBefore = await context.Product.CountAsync();
+            ProductController productController = new ProductController(productService, languageService, _localizerController);
 
-        // Création du produit qui devra être supprimé
-        productController.Create(productViewModel);
-        var product = await context.Product
-            .Where(p => p.Name == "Le produit a supprimer")
-            .FirstOrDefaultAsync();
+            ProductViewModel productViewModel = new ProductViewModel()
+            {
+                Name = "Le produit a supprimer",
+                Price = "9.99",
+                Stock = "10",
+                Description = "La description du produit",
+                Details = "Les détails du produit"
+            };
 
-        // Act
-        productController.DeleteProduct(product.Id);
+            int productsBefore = await context.Product.CountAsync();
 
-        // Assert
-        Assert.Equal(productsBefore, context.Product.Count());
+            productController.Create(productViewModel);
+            product = await context.Product
+                .Where(p => p.Name == "Le produit a supprimer")
+                .FirstOrDefaultAsync();
 
-        var productHereAgain = await context.Product
-            .Where(p => p.Name == "Le produit a supprimer")
-            .FirstOrDefaultAsync();
-        Assert.Null(productHereAgain);
+            // Act
+            productController.DeleteProduct(product.Id);
+
+            // Assert
+            Assert.Equal(productsBefore, context.Product.Count());
+
+            var productHereAgain = await context.Product
+                .Where(p => p.Name == "Le produit a supprimer")
+                .FirstOrDefaultAsync();
+            Assert.Null(productHereAgain);
+        }
+        finally
+        {
+            if (product != null)
+            {
+                var productHereAgain = await context.Product
+                .Where(p => p.Name == "Le produit a supprimer")
+                .FirstOrDefaultAsync();
+
+                if (productHereAgain != null)
+                    await CleanTestData(context, product);
+            }
+        }
     }
 
     private P3Referential CreateDbContext()
@@ -131,18 +135,14 @@ public class ProductIntegrationTests
 
     private async Task CleanTestData(P3Referential context, Product product)
     {
-        // Suppression des OrderLine liées au produit du test
         var lines = context.OrderLine.Where(ol => ol.ProductId == product.Id);
         context.OrderLine.RemoveRange(lines);
 
-        // Suppression des Order qui n'ont plus de lignes
         var emptyOrders = context.Order.Where(o => !context.OrderLine.Any(ol => ol.OrderId == o.Id));
         context.Order.RemoveRange(emptyOrders);
 
-        // Suppression du produit du test
         context.Product.Remove(product);
 
-        // Envoie de toutes les suppressions à SQL Server
         await context.SaveChangesAsync();
     }
 }
